b1bc5ae2cceee06f8cddbeb77335d87fea73da48
bjudas <ben.judas03@gmail.com>
2019-04-03 17:03:41 -0700

    Push for Asign #1

--- index.js
+++ index.js
@@ -1,8 +1,30 @@
-var express = require('express');
-var app = express();
+const http = require('http');
+const fs = require('fs');
 
-app.get('/', function(req, res) {
-  res.send("Hello World");
-})
-
-app.listen(3000);
+http.createServer(function(req,res) {
+  const path = req.url.toLowerCase();
+  switch(path) {
+    case '/':
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      console.log(path);
+      fs.readFile('public/home.html', function(err, data) {
+        if (err) return err;
+        res.write(data);
+        res.end();
+      })
+      break;
+    case '/about':
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      console.log(path);
+      fs.readFile('public/about.html', function(err, data) {
+        if (err) return err;
+        res.write(data);
+        res.end();
+      })
+      break;
+    default:
+      res.writeHead(404, {'Content-Type': 'text/html'});
+      res.end("<h1>No. I don't wanna show you this. Bleargh!</h1>")
+      break;
+  };
+}).listen(process.env.PORT || 3000);
